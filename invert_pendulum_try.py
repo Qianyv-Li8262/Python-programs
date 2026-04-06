559aa0a78dc635bd6edf918a8ad9e16cefe352c2
Qianyv_Li_8262 <3835917369@qq.com>
2026-04-06 22:29:46 +0800

    the curriculum feature fully successed.committing.

--- invert_pendulum_try.py
+++ invert_pendulum_try.py
@@ -31,54 +31,109 @@ def getDerivative(y,f):
 def rk2solver(y0,dt,f):
     ymid=y0+dt/2*getDerivative(y0,f)
     return y0+dt*getDerivative(ymid,f)
+
+
+
+
+
 def loss(y,f):
-    return 0.01*y[0]**2+(1-torch.cos(y[1]))*2+0.001*y[2]**2+0.001*y[3]**2+0.001*f**2
+    e=0.015
+    # return torch.sqrt(y[0]**2+e**2)*0.1+torch.relu(y[0]**2-16)+(1-torch.cos(y[1]))*2+0.001*y[2]**2+0.001*y[3]**2+0.001*f**2
+    return y[0]**2*0.5+torch.relu(y[0]**2-16)+(1-torch.cos(y[1]))*5+0.05*y[2]**2+0.5*y[3]**2+0.005*f**2
+
+
+
+
 net = VerySimpleCar().to(device)
 
-optimizer = optim.Adam(net.parameters(), lr=0.0005)
+optimizer = optim.Adam(net.parameters(), lr=0.001)
 dt = 0.05  # 时间步长
 
-steps=100
-times=20
+steps=60
+times=60
+
+
 # y0 = torch.tensor([0.8, 0.7, 0.0, 0.0], dtype=torch.float32, requires_grad=False,device=device)
+
+
+
 
 class CurriculumManager:
     def __init__(self):
-        self.level = 0  # 难度等级 0 ~ 10
+        self.level = 0
         self.max_level = 10
-        self.loss_window = [] # 存储最近的 loss
+        self.loss_window = []
 
-    def get_init_state(self, device):
-        # 根据 level 动态计算噪声强度
-        # Level 0: theta 扰动 0.1 (维稳)
-        # Level 10: theta 扰动 3.14 (起摆)
-        th_scale = 0.1 + (self.level / self.max_level) * 3.04
+    # def get_init_state(self, device,debug=-1):
+    #     a=self.level
+    #     if debug!=-1:
+    #         self.level=debug
+    #     th_scale = 0.1 + (self.level / self.max_level) * 3.04
+    #     x_scale = 0.1 + (self.level / self.max_level) * 2.0
+    #     self.level=a
+    #     noise = (torch.rand(4, device=device) - 0.5) * 2.0 * \
+    #             torch.tensor([x_scale, th_scale, 0.1, 0.1], device=device)
+    #     return torch.tensor([0.0, 0.0, 0.0, 0.0], device=device) + noise
+
+
+    def get_init_state(self,device,debug=-1):
+        a=self.level
+        if debug!=-1:
+            self.level=debug
         x_scale = 0.1 + (self.level / self.max_level) * 2.0
-        
-        noise = (torch.rand(4, device=device) - 0.5) * 2.0 * \
-                torch.tensor([x_scale, th_scale, 0.1, 0.1], device=device)
-        
-        # 初始状态在 [0, 0, 0, 0] 基础上加噪声
-        return torch.tensor([0.0, 0.0, 0.0, 0.0], device=device) + noise
+        x_pos = (torch.rand(1, device=device) - 0.5) * 2.0 * x_scale
+        if self.level<=3:
+            th_scale = 0.1 + (self.level / self.max_level) * 3.04
+            th_pos = (torch.rand(1, device=device) - 0.5) * 2.0 * th_scale
+        elif self.level<=8:
+            min_th = 1.0 + ((self.level - 5) / 3.0) * 1.0
+            max_th = 2.0 + ((self.level - 5) / 3.0) * 1.0
+            sign = 1.0 if torch.rand(1).item() > 0.5 else -1.0
+            th_pos = sign * (min_th + torch.rand(1, device=device) * (max_th - min_th))
+        else:
+            sign = 1.0 if torch.rand(1).item() > 0.5 else -1.0
+            th_pos = sign * 3.14 + (torch.rand(1, device=device) - 0.5) * 0.2
 
-    def update(self, current_loss):
-        self.loss_window.append(current_loss)
-        if len(self.loss_window) > 20:
-            self.loss_window.pop(0)
-            avg_loss = sum(self.loss_window) / 20
+        v_noise = (torch.rand(2, device=device) - 0.5) * 0.2
+        
+        self.level = a
+        
+        return torch.tensor([
+            x_pos.item(), 
+            th_pos.item(), 
+            v_noise[0].item(), 
+            v_noise[1].item()
+        ], device=device)
+
+
+    def update(self, current_loss,u,times):
+        if u>times//2:
+            self.loss_window.append(current_loss)
+            if len(self.loss_window) > 20:
+                self.loss_window.pop(0)
+                avg_loss = sum(self.loss_window) / 20
             
-            # 如果平均误差足够小，且还没到最高级，则升级
-            if avg_loss < 0.05 and self.level < self.max_level:
-                self.level += 1
-                self.loss_window = [] # 清空窗口重新评估新难度
-                print(f"--- 难度升级！当前等级: {self.level} ---")
-            
-            # 如果误差突然变得巨大（学崩了），可以考虑降级保护
-            elif avg_loss > 5.0 and self.level > 0:
-                self.level -= 1
-                self.loss_window = []
-                print(f"--- 难度降级保护! 当前等级: {self.level} ---")
+                # 如果平均误差足够小，且还没到最高级，则升级
+                if avg_loss < 0.01 and self.level < self.max_level:
+                    self.level += 1
+                    self.loss_window = [] # 清空窗口重新评估新难度
+                    print(f"!!!!!!!!!!!!! 难度升级！当前等级: {self.level} !!!!!!!!!!!!!")
+
+                # 如果误差突然变得巨大（学崩了），可以考虑降级保护
+                elif avg_loss > 5.0 and self.level > 0:
+                    self.level -= 1
+                    self.loss_window = []
+                    print(f"!!!!!!!!!!!!! 难度降级保护! 当前等级: {self.level} !!!!!!!!!!!!!")
+        elif u==0:
+            self.loss_window=[]
+
+
+
+
 manager=CurriculumManager()
+
+
+
 def sim_step(y0,dt,step):
     ttlos = torch.tensor(0.0, device=y0.device)
     for i in range(step):
@@ -90,22 +145,24 @@ def sim_step(y0,dt,step):
 compiled_step = torch.compile(sim_step, mode="reduce-overhead")
 u=0
 y0=manager.get_init_state(device)
-for epoch in range(1500):
+
+
+for epoch in range(1100):
     optimizer.zero_grad()
     ttloss=0
-    y,ttloss=compiled_step(y0,dt,steps)
+    # y,ttloss=compiled_step(y0,dt,steps)
+    y,ttloss=sim_step(y0,dt,steps)
     u+=1
     y0=y.clone().detach()
-    if ttloss>10 or abs(y[1])>10 or abs(y[0])>10 or abs(y[2])>5 or u>=times:
-        # noise = (torch.rand(4, device=device)-0.5) * torch.tensor([1, 1, 0.05, 0.05], device=device)
-        # y0 = torch.tensor([0.0, 0.0, 0.0, 0.0], device=device) + noise
+    if ttloss>20 or abs(y[1])>10 or abs(y[0])>10 or abs(y[2])>10:
+        y0=manager.get_init_state(device)
+        u=0
+        print('---OutBoundReset---')
+    if u>=times:
         y0=manager.get_init_state(device)
         u=0
         print('---Reset---')
     if torch.isnan(ttloss) or torch.isinf(ttloss) or torch.isnan(y).any():
-        # print(y)
-        # noise = (torch.rand(4, device=device)-0.5) *2* torch.tensor([1, 1, 0.05, 0.05], device=device)
-        # y0 = torch.tensor([0.0, 0.0, 0.0, 0.0], device=device) + noise
         y0=manager.get_init_state(device)
         u=0
         print('---Force Reset---')
@@ -113,12 +170,15 @@ for epoch in range(1500):
     ttloss.backward()
     torch.nn.utils.clip_grad_norm_(net.parameters(), 1.0)
     optimizer.step()
-    if epoch%50==0:
-        print(ttloss)
-        print(y)
+    # if epoch%50==0:
+    #     print(y)
     # print(F)
-    # print(epoch)
-    manager.update(ttloss.item())
+    print(epoch)
+    print(f"Level: {manager.level}")
+    print(f'u={u}')
+    print(ttloss.item())
+    print(y)
+    manager.update(ttloss.item(),u,times)
 # 将模型的权重保存为一个 .pth 文件
 torch.save(net.state_dict(), "pendulum_controller_curriculum.pth")
 print("权重已成功保存！")
