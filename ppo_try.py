f666cf0f34ef420a65ca80c42cb9e0d3711a25cb
Qianyv_Li_8262 <3835917369@qq.com>
2026-04-08 18:34:35 +0800

    runable but very slow

--- ppo_try.py
+++ ppo_try.py
@@ -47,8 +47,8 @@ def rk2solver(y0,dt,f):
 
 def reward(y,f):
     # f=f.view_as(y[...,0])
-    return 1.0-(y[...,0]**2*0.5+torch.relu(y[...,0]**2-16)*1.5+(1-torch.cos(y[...,1]))*5+0.05*y[...,2]**2+0.6*y[...,3]**2+0.005*f**2)
-class ActorCritic(nn.Module):
+    return 1.0-(y[...,0]**2*0.5+torch.relu(y[...,0]**2-16)*1.5+(1-torch.cos(y[...,1]))*8+0.05*y[...,2]**2+1*y[...,3]**2+0.005*f**2)
+class Actor(nn.Module):
     def __init__(self,indim):
         super().__init__()
         self.rootnet=nn.Sequential(
@@ -61,33 +61,59 @@ class ActorCritic(nn.Module):
             nn.Linear(256,1),
             nn.Tanh()
         )
-        self.critic=nn.Sequential(
-            nn.Linear(256,1)
-        )
+        # self.critic=nn.Sequential(
+        #     nn.Linear(256,1)
+        # )
         self.log_std=nn.Parameter(torch.zeros(1))
     def forward(self,state)-> tuple[distributions.Normal, torch.Tensor]:
         feat=self.rootnet(state)
         mu=10*self.actor(feat)
+        # v=self.critic(feat)
+        # std=torch.exp(self.log_std)
+        # dist=distributions.Normal(mu,std)
+        return mu,self.log_std
+class Critic(nn.Module):
+    def __init__(self,indim):
+        super().__init__()
+        self.rootnet=nn.Sequential(
+                nn.Linear(indim,256),
+                nn.LeakyReLU(),
+                nn.Linear(256,256),
+                nn.LeakyReLU()
+        )
+        self.critic=nn.Sequential(
+            nn.Linear(256,1),
+        )
+        # self.critic=nn.Sequential(
+        #     nn.Linear(256,1)
+        # )
+        self.log_std=nn.Parameter(torch.zeros(1))
+    def forward(self,state)-> tuple[distributions.Normal, torch.Tensor]:
+        feat=self.rootnet(state)
         v=self.critic(feat)
         # std=torch.exp(self.log_std)
         # dist=distributions.Normal(mu,std)
-        return mu,self.log_std,v.squeeze(-1)
+        return v.squeeze(-1)
 
 def chkdeath(y):
     x=torch.abs(y[...,0])>5.0
-    return x.float()
+    y=torch.abs(y[...,1])>6.28
+    z=x or y
+    return z.float()
 
-mainNetwork=ActorCritic(7).to(device)
-mainNetwork=torch.compile(mainNetwork,mode="reduce-overhead")
+mainNetwork=Actor(7).to(device)
+# mainNetwork=torch.compile(mainNetwork,mode="reduce-overhead")
+mainNetwork2=Critic(7).to(device)
+# mainNetwork2=torch.compile(mainNetwork2,mode="reduce-overhead")
 optimizer=optim.Adam(mainNetwork.parameters(),lr=0.0003)
+optimizer2=optim.Adam(mainNetwork2.parameters(),lr=0.0003)
 
-sample_batch_size=256
-N=8 
-#先不加for循环
+sample_batch_size=2048
+N=2 
 level=0
 max_level=10
 dt=0.05
-steps=50
+steps=200
 
 def get_init(batch_size):
     x_scale = 0.1 + (level / max_level) * 2.0
@@ -100,8 +126,9 @@ def get_init(batch_size):
 # buffer=[]
 gamma=0.95
 lamb=0.95
-K=10
-mini_batch_size=1024
+K=5
+MAX_EPISODE_NUMBER=75
+mini_batch_size=2048
 e=0.2
 c1=0.5
 c2=0.01
@@ -153,14 +180,30 @@ def sample_step(y0, net_mu, net_lgstd):
     next_state=rk2solver(y0,dt,f_sq)
     rwd=reward(next_state,f_sq)
     mask_death=(torch.abs(next_state[...,0])>5.0).float()
+    rwd = rwd - mask_death * 100.0
     return f_sq, log_prob.squeeze(-1), next_state, rwd, mask_death
 
 
+@torch.compile(mode='reduce-overhead')
+def compute_loss(state_b,returns_b,action_b,log_prob_b,A_b):    
+    muu,lggstd=mainNetwork(state_b)
+    muu=muu.squeeze(-1)
+    V_pred=mainNetwork2(state_b)
+    new_log_probs=-((action_b-muu)**2)/(2*torch.exp(2*lggstd))-lggstd-0.9189385332
+    ratio=torch.clamp(torch.exp(new_log_probs-log_prob_b),0,5)
+    surr1=ratio*A_b
+    surr2=torch.clamp(ratio,1-e,1+e)*A_b
+    actor_loss=-torch.mean(torch.min(surr1,surr2))
+    critic_loss=torch.mean((V_pred-returns_b)**2)
+    entropy_loss=-torch.mean(lggstd)-1.418938533
+    return actor_loss + c1 * critic_loss + c2 * entropy_loss
 
 
-MAX_EPISODE_NUMBER=3
+
+
 # -------采样--------
 for _ in range(MAX_EPISODE_NUMBER):
+    print(f'-------Episode {_}-------')
     print('sampling start.')
     with torch.no_grad():
         for n in range(N):
@@ -169,7 +212,8 @@ for _ in range(MAX_EPISODE_NUMBER):
             for i in range(steps):
                 # torch.compiler.cudagraph_mark_step_begin() 
                 state_in=torch.stack([y0[:,0],torch.sin(y0[:,1]),torch.cos(y0[:,1]),torch.sin(2*y0[:,1]),torch.cos(2*y0[:,1]),y0[:,2],y0[:,3]],dim=-1)
-                mu,lgstd,V=mainNetwork(state_in)
+                mu,lgstd=mainNetwork(state_in)
+                V=mainNetwork2(state_in)
                 # f=torch.clamp(mu+torch.randn_like(mu)*torch.exp(lgstd),-15,15)
                 # log_prob=-((f-mu)**2)/(2*torch.exp(2*lgstd))-lgstd-0.9189385332
                 # # f=dist.sample().squeeze(-1)   #f (256, )
@@ -200,13 +244,13 @@ for _ in range(MAX_EPISODE_NUMBER):
             final_state_in=torch.stack([final_state_raw[:,0],torch.sin(final_state_raw[:,1]),
                                     torch.cos(final_state_raw[:,1]),torch.sin(2*final_state_raw[:,1]),
                                     torch.cos(2*final_state_raw[:,1]),final_state_raw[:,2],final_state_raw[:,3]],dim=-1)
-            _,__,Vp=mainNetwork(final_state_in)
+            Vp=mainNetwork2(final_state_in)
             A=torch.zeros(sample_batch_size,device=device)
             for t in reversed(range(steps)):
         # V_next=traj[t+1]['V'] if t+1<steps else Vp
                 V_next=buffer['V'][...,n*steps+t+1] if t+1<steps else Vp
                 delta=buffer['reward'][...,n*steps+t]+gamma*(1-buffer['death'][...,n*steps+t])*V_next-buffer['V'][...,n*steps+t]
-                A=delta+gamma*lamb*A*(1-buffer['death'][:,n*steps+t])-buffer['death'][:,n*steps+t]*100.0
+                A=delta+gamma*lamb*A*(1-buffer['death'][:,n*steps+t])
                 buffer['A'][:,n*steps+t]=A
                 buffer['R'][:,n*steps+t]=A+buffer['V'][:,n*steps+t]
 
@@ -219,25 +263,34 @@ for _ in range(MAX_EPISODE_NUMBER):
 
 
     for epoch in range(K):
-        print(f'epoch {epoch} started.')
+        
         buffer_shfled={}
         shfl_idx=torch.randperm(N*sample_batch_size*steps,device=device)
         for key,value in buffer.items():
             buffer_shfled[key]=shfl(reshap(value),shfl_idx)
+        ls=[]
         for i in range(u):
-            muu,lggstd,V_pred=mainNetwork(buffer_shfled['state'][i*mini_batch_size:(i+1)*mini_batch_size,:])
-            returns=buffer_shfled['R'][i*mini_batch_size:(i+1)*mini_batch_size]
-            old_action=buffer_shfled['action'][i*mini_batch_size:(i+1)*mini_batch_size]
-            # new_log_probs=dis.log_prob(old_action.unsqueeze(-1)).squeeze(-1)
-            new_log_probs=-((old_action-muu)**2)/(2*torch.exp(2*lggstd))-lggstd-0.9189385332
-            ratio=torch.clamp(torch.exp(new_log_probs-buffer_shfled['log_prob'][i*mini_batch_size:(i+1)*mini_batch_size]),0,5)
-            surr1=ratio*buffer_shfled['A'][i*mini_batch_size:(i+1)*mini_batch_size]
-            surr2=torch.clamp(ratio,1-e,1+e)*buffer_shfled['A'][i*mini_batch_size:(i+1)*mini_batch_size]
-            actor_loss=-torch.mean(torch.min(surr1,surr2))
-            critic_loss=torch.mean((V_pred-returns)**2)
-            entropy_loss=-torch.mean(lggstd)-1.418938533
-            total_loss = actor_loss + c1 * critic_loss + c2 * entropy_loss
+            start = i * mini_batch_size
+            end = (i + 1) * mini_batch_size
+            b_state=buffer_shfled['state'][start:end,:]
+            # muu,lggstd,V_pred=mainNetwork(b_state)
+            b_returns=buffer_shfled['R'][start:end]
+            b_actions=buffer_shfled['action'][start:end]
+            b_old_log_prob=buffer_shfled['log_prob'][start:end]
+            b_A=buffer_shfled['A'][start:end]
+            total_loss=compute_loss(b_state,b_returns,b_actions,b_old_log_prob,b_A)
+            # old_action=b_actions
+            # # new_log_probs=dis.log_prob(old_action.unsqueeze(-1)).squeeze(-1)
+            # new_log_probs=-((old_action-muu)**2)/(2*torch.exp(2*lggstd))-lggstd-0.9189385332
+            # ratio=torch.clamp(torch.exp(new_log_probs-b_old_log_prob),0,5)
+            # surr1=ratio*b_A
+            # surr2=torch.clamp(ratio,1-e,1+e)*b_A
+            # actor_loss=-torch.mean(torch.min(surr1,surr2))
+            # critic_loss=torch.mean((V_pred-returns)**2)
+            # entropy_loss=-torch.mean(lggstd)-1.418938533
+            # total_loss = actor_loss + c1 * critic_loss + c2 * entropy_loss
             optimizer.zero_grad(set_to_none=True)
+            optimizer2.zero_grad(set_to_none=True)
             # if torch.isnan(total_loss) or torch.isinf(total_loss):
             #     print('NaN detected,skipping batch.')
             #     optimizer.zero_grad(set_to_none=True)
@@ -246,5 +299,14 @@ for _ in range(MAX_EPISODE_NUMBER):
             total_loss.backward()
             torch.nn.utils.clip_grad_norm_(mainNetwork.parameters(), 0.5)  # 梯度裁剪
             optimizer.step()
-torch.save(mainNetwork._orig_mod.state_dict(), "pendulum_controller_ppo.pth")
+            optimizer2.step()
+            ls.append(total_loss.item())
+            # print(total_loss.item())
+        v=sum(ls)/u
+        print(f'epoch {epoch} ,loss {v}')
+    uu=buffer['reward'].mean().item()
+    print(f'\nAverage Reward={uu}')
+    if uu>0.96:
+        level+=1
+torch.save(mainNetwork.state_dict(), "pendulum_controller_ppo.pth")
 print("权重已成功保存！")
